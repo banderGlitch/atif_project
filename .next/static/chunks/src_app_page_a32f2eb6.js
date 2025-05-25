13ebe05eb5fa6b8faad7fa70898da485309524ab
Mohd Akib <mohd.akib@lumiq.ai>
2025-05-25 12:43:34 +0530

    .next deleted

--- .next/static/chunks/src_app_page_a32f2eb6.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_f937cc19._.css",
-  "static/chunks/_650ae728._.js"
-],
-    source: "dynamic"
-});
