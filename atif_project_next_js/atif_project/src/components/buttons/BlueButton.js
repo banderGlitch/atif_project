c9690f0e0e4ff4de4d0221b824f9416e6258ad43
Mohd Akib <mohd.akib@lumiq.ai>
2025-05-25 12:36:28 +0530

    atif folder deleted

--- atif_project_next_js/atif_project/src/components/buttons/BlueButton.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const BlueButton = ({text}) => {
-  return (
-    <button className="cta-button cta-talk">{text}</button>
-  )
-}
--- atif_project_next_js/atif_project/src/components/buttons/GreenButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const GreenButton = ({text}) => {
-    return (
-        <div><button className="cta-button cta-buy">{text}</button></div>
-    )
-}
-
-export default GreenButton
